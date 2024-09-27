48ec28910639768314b46d0ca6d2392336a320ee
Aaron Williams <aaronv2.aw@gmail.com>
2024-09-27 08:47:30 -0400

    All done

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,40 +1,27 @@
-public class ListNode {
-    public int val;
-    public ListNode next;
-    
-    public ListNode(int x) { 
-        val = x; 
-    }
-}
+public class Problem1 {
 
-public class LinkedListProblems {
-
-    // Function to concatenate two linked lists
     public static ListNode concatenate(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return copyList(l2);
         }
 
         ListNode current = l1;
-        
-        // Traverse to the end of the first list
+
         while (current.next != null) {
             current = current.next;
         }
 
-        // Create a copy of l2 and link it to the end of l1
         ListNode l2Copy = copyList(l2);
         current.next = l2Copy;
 
         return l1;
     }
 
-    // Helper function to create a copy of a linked list
     private static ListNode copyList(ListNode node) {
         if (node == null) {
             return null;
         }
-        
+
         ListNode head = new ListNode(node.val);
         ListNode current = head;
         node = node.next;
@@ -47,4 +34,36 @@ public class LinkedListProblems {
 
         return head;
     }
+
+    
+    public static void main(String[] args) {
+    
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(1);
+
+    
+        ListNode l2 = new ListNode(4);
+        l2.next = new ListNode(5);
+
+        System.out.println("Original List l1: " + listToString(l1));
+        System.out.println("Original List l2: " + listToString(l2));
+
+    
+        ListNode concatenatedList = concatenate(l1, l2);
+        System.out.println("Concatenated List: " + listToString(concatenatedList));
+    }
+
+    public static String listToString(ListNode node) {
+        StringBuilder sb = new StringBuilder();
+        while (node != null) {
+            sb.append(node.val);
+            if (node.next != null) {
+                sb.append(" -> ");
+            }
+            node = node.next;
+        }
+        return sb.toString();
+    }
 }
+
