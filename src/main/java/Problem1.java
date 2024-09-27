772cb9ca4a19f6059a3ad703ce48aaf4640b9e81
AaronIGW <85689101+AaronIGW@users.noreply.github.com>
2024-09-27 08:10:53 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,9 +1,50 @@
-// Problem1.java
-public class Problem1 {
+public class ListNode {
+    public int val;
+    public ListNode next;
+    
+    public ListNode(int x) { 
+        val = x; 
+    }
+}
+
+public class LinkedListProblems {
+
+    // Function to concatenate two linked lists
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        // TODO: Implement the concatenate method
-        // This method should concatenate l2 to l1 and return l1
-        // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        if (l1 == null) {
+            return copyList(l2);
+        }
+
+        ListNode current = l1;
+        
+        // Traverse to the end of the first list
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        // Create a copy of l2 and link it to the end of l1
+        ListNode l2Copy = copyList(l2);
+        current.next = l2Copy;
+
+        return l1;
+    }
+
+    // Helper function to create a copy of a linked list
+    private static ListNode copyList(ListNode node) {
+        if (node == null) {
+            return null;
+        }
+        
+        ListNode head = new ListNode(node.val);
+        ListNode current = head;
+        node = node.next;
+
+        while (node != null) {
+            current.next = new ListNode(node.val);
+            current = current.next;
+            node = node.next;
+        }
+
+        return head;
     }
 }
